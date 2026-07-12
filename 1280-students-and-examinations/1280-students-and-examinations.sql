6e6aec12b86be7dbf2fc60825200bde3067614aa
prachi upreti <prachiupreti2108@gmail.com>
2026-07-12 08:59:04 +0530

    Time: 977 ms (80.6%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -2,7 +2,7 @@ SELECT
     Students.student_id,
     Students.student_name,
     Subjects.subject_name,
-    COUNT(Examinations.student_id) AS attended_exams
+    COUNT(Examinations.subject_name) AS attended_exams
 FROM Students
 CROSS JOIN Subjects
 LEFT JOIN Examinations
